b97abc5753e2d6353661bcc239983aed17ab7778
Your Name <shapettt@gmail/com>
2020-05-07 21:37:57 +0300

    sec3

--- dz_5_lesson.py
+++ dz_5_lesson.py
@@ -20,4 +20,24 @@ if a > 50 and  (b > a or c > a):
 if a > 5 or (b == 7 and c == 7):      
     print('Петя')
 
+
     # ДЗ
+a = int(input('Введите число : '))
+b = int(input('Введите число : '))
+c = int(input('Введите число : '))
+
+
+g = (a == 0 and b == 0 and c == 0 and 'Введены все нули') or ""
+print(g)
+v = (a != 0 and b != 0  and c != 0) and 'Нет нулевых значений!!!' or ''
+print(v)
+z = a or b or c or ''
+print(z)
+if a > (b + c):
+    print(a - b - c)
+elif a < (b + c):
+    print(b + c - a)            
+if a > 50 and  (b > a or c > a):0
+print("Вася")
+if a > 5 or (b == 7 and c == 7):      
+    print('Петя')
